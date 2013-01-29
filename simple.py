1c89307ea1991f9ef2f379418911ecf0c7fc5d8e
Andrew Lenards <lenards@iplantcollaborative.org>
2013-01-29 16:52:00 -0700

    next version - more sophistication... optparser FTW

--- simple.py
+++ simple.py
@@ -1,11 +1,32 @@
+# 
 
 
-fname = 'test-set.txt'
+from optparse import OptionParser
 
-with open(fname) as f:
-    content = f.readlines()
+def get_args():
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="filename",
+              help="the file, FILE, read from...", metavar="FILE")
 
-for line in content:
-    print line.strip()
+    return parser.parse_args()
 
+def grab_file(options, args):
+    if (options.filename == None):
+        return args[0]
+    return options.filename
+    
+def main():
+    (options, args) = get_args()
+
+    fname = grab_file(options, args)
+
+    with open(fname) as f:
+        content = f.readlines()
+
+    for line in content:
+        print line.strip()
+
+
+if __name__ == "__main__":
+    main()
 
